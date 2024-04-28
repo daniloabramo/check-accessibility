9c0c9acd7a0181696cb9682873afc1b89d9b8762
daniloabramo <142537432+daniloabramo@users.noreply.github.com>
2024-04-28 03:42:03 -0300

    first commit

---
+++ script/menu.js
@@ -0,0 +1,4 @@
+document.getElementById("navButton").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active"); // Adiciona ou remove a classe active
+});
